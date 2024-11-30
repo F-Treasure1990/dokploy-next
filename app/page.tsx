417d5db2781fcc25edb492d17acfc2ffd08a1f03
Fenton <fentontreasure@gmail.com>
2024-11-30 18:08:00 +0000

    PPR test

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
 import Image from "next/image";
+import { Suspense } from "react";
+
+async function SlowComponent() {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  return <div>This is a dynamic (and slow) route</div>;
+}
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <Suspense fallback={<div>Loading..</div>}>
+          <SlowComponent />
+        </Suspense>
         <Image
           className="dark:invert"
           src="/next.svg"
